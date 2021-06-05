1c09a5c207c6d5feb88b47c7ed9e3e3426d3c3b1
Shehraan2008 <shehraanrathee21@gmail.com>
2021-06-05 18:53:05 +0530

    Adding files to github

---
+++ sketch.js
@@ -0,0 +1,16 @@
+// Alias for Matter.js
+const { Engine, World, Bodies, Body, Constraint } = Matter;
+let engine, world;
+
+// Variables.
+
+// Loading Image Assets.
+function preload(params) {}
+
+function setup(params) {
+  const canvas = createCanvas(windowWidth - 10, windowHeight - 10);
+}
+
+function draw(params) {
+  background(0);
+}
